dda4b8a6bfe739b08a6dad7674fb4b9e187b7d50
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-09-07 14:23:32 +0200

    test

--- quickstart1_app.py
+++ quickstart1_app.py
@@ -1,3 +1,29 @@
 import streamlit
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
 
 streamlit.title('Hey you')
+
+# Setup for Storytelling (matplotlib):
+plt.rcParams['font.family'] = 'monospace'
+plt.rcParams['font.size'] = 8
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['figure.facecolor'] = '#464545' 
+plt.rcParams['axes.facecolor'] = '#464545' 
+plt.rcParams['axes.titleweight'] = 'bold'
+plt.rcParams['axes.titlecolor'] = 'black'
+plt.rcParams['axes.titlesize'] = 9
+plt.rcParams['axes.labelcolor'] = 'darkgray'
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.edgecolor'] = 'darkgray'
+plt.rcParams['axes.linewidth'] = 0.2
+plt.rcParams['ytick.color'] = 'darkgray'
+plt.rcParams['xtick.color'] = 'darkgray'
+plt.rcParams['axes.titlecolor'] = '#FFFFFF'
+plt.rcParams['axes.titlecolor'] = 'white'
+plt.rcParams['axes.edgecolor'] = 'darkgray'
+plt.rcParams['axes.linewidth'] = 0.85
+plt.rcParams['ytick.major.size'] = 0
